20423f86425103665fbf251365fcaff1f3879cfa
junghyunbak <jeong5728@gmail.com>
2023-10-11 18:25:22 +0900

    feat: 네비게이션 버튼 컴포넌트 생성

---
+++ src/components/NavigationButton.tsx
@@ -0,0 +1,44 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  GestureResponderEvent,
+} from "react-native";
+
+interface NavigationButtonProps {
+  fontColor?: string;
+  backgroundColor?: string;
+  content?: string;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+export function NavigationButton({
+  backgroundColor = "#8579F1",
+  fontColor = "white",
+  content,
+  onPress,
+}: NavigationButtonProps) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <Text style={[styles.buttonText, { color: fontColor }]}>{content}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 48,
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontFamily: "Pretendard-SemiBold",
+  },
+});
