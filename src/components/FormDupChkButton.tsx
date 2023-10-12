8ee82ef9285a92117c14aa49a15f1e080a4ac8cb
junghyunbak <jeong5728@gmail.com>
2023-10-13 03:41:02 +0900

    style: 중복 체크 버튼 스타일링

--- src/components/FormDupChkButton.tsx
+++ src/components/FormDupChkButton.tsx
@@ -11,27 +11,34 @@ interface FormDupChkButtonProps extends TouchableOpacityProps {
 }
 
 export function FormDupChkButton({ isDup, ...props }: FormDupChkButtonProps) {
-  const stateColor =
-    isDup === null ? "#B7B7CB" : !isDup ? "#63BCA9" : "#FF8585";
+  const fontColor = isDup === null ? "#B7B7CB" : !isDup ? "#63BCA9" : "#FF8585";
 
-  console.log(stateColor);
+  const borderColor =
+    isDup === null ? "transparent" : !isDup ? "#63BCA9" : "#FF8585";
+
+  const BORDER_WIDTH = 2;
 
   return (
     <TouchableOpacity {...props}>
       <View
         style={{
-          paddingHorizontal: 18,
-          paddingVertical: 10,
+          /**
+           * native는 box-sizing 속성이 border-box 처럼 동작하지만 고정 height를 주지 않았기 때문에
+           * 테두리 두께에 따라 크기가 달라지므로 테두리 두께 값을 적절히 빼 줌
+           */
+          paddingHorizontal: 18 - BORDER_WIDTH,
+          paddingVertical: 10 - BORDER_WIDTH,
           backgroundColor: "#EBEBF0",
           borderRadius: 4,
-          borderColor: stateColor,
+          borderWidth: BORDER_WIDTH,
+          borderColor: borderColor,
         }}
       >
         <Text
           style={{
             fontSize: 16,
             fontFamily: "Pretendard-SemiBold",
-            color: stateColor,
+            color: fontColor,
           }}
         >
           중복확인
