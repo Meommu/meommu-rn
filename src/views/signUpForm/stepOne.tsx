5b58422fe55912dbe4fc9e52449d52138a20a6f1
junghyunbak <jeong5728@gmail.com>
2023-10-13 19:21:07 +0900

    style: 회원가입 첫번째 폼 스타일 정리

--- src/views/signUpForm/stepOne.tsx
+++ src/views/signUpForm/stepOne.tsx
@@ -1,5 +1,5 @@
 import { Controller } from "react-hook-form";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useState } from "react";
 import { FormInput } from "../../components/FormInput";
 import { FormDupChkButton } from "../../components/FormDupChkButton";
@@ -43,48 +43,29 @@ export function StepOne({ control, setValue, errors }: StepOneProps) {
   };
 
   return (
-    <View
-      style={{
-        gap: 18,
-        paddingHorizontal: 20,
-        height: "100%",
-        width: "100%",
-      }}
-    >
+    <View style={styles.container}>
       {/**
        * 이메일
        */}
-      <View style={{ gap: 12 }}>
-        <View style={{ flexDirection: "row", gap: 12 }}>
-          <Text
-            style={{
-              fontSize: 14,
-              fontFamily: "Pretendard-SemiBold",
-            }}
-          >
-            이메일 주소를 입력해주세요
-          </Text>
-          <Text>{JSON.stringify(errors.email, null, 2)}</Text>
-          {errors.email && (
-            <Text>
-              이메일 유효성 검사 통과 못함{" "}
-              {JSON.stringify(errors.email, null, 2)}
-            </Text>
-          )}
+      <View style={styles.fieldView}>
+        <View style={styles.fieldTextLayoutView}>
+          <Text style={styles.fieldGuideText}>이메일 주소를 입력해주세요</Text>
+
           {isEmailDup !== null && (
             <Text
-              style={{
-                fontSize: 14,
-                fontFamily: "Pretendard-SemiBold",
-                color: isEmailDup ? "#FF8585" : "#63BCA9",
-              }}
+              style={[
+                styles.fieldAlertText,
+                {
+                  color: isEmailDup ? "#FF8585" : "#63BCA9",
+                },
+              ]}
             >
               사용 {isEmailDup ? "불" : ""}가능한 이메일 입니다.
             </Text>
           )}
         </View>
 
-        <View style={{ flexDirection: "row", gap: 15 }}>
+        <View style={styles.emailFieldControllerLayoutView}>
           <Controller
             name="email"
             control={control}
@@ -114,24 +95,18 @@ export function StepOne({ control, setValue, errors }: StepOneProps) {
       {/**
        * 패스워드
        */}
-      <View style={{ gap: 12 }}>
-        <View style={{ flexDirection: "row", gap: 12 }}>
-          <Text
-            style={{
-              fontSize: 14,
-              fontFamily: "Pretendard-SemiBold",
-            }}
-          >
-            비밀번호를 입력해주세요
-          </Text>
+      <View style={styles.fieldView}>
+        <View style={styles.fieldTextLayoutView}>
+          <Text style={styles.fieldGuideText}>비밀번호를 입력해주세요</Text>
 
           {isEmailDup !== null && (
             <Text
-              style={{
-                fontSize: 14,
-                fontFamily: "Pretendard-SemiBold",
-                color: isEmailDup ? "#FF8585" : "#63BCA9",
-              }}
+              style={[
+                styles.fieldAlertText,
+                {
+                  color: isEmailDup ? "#FF8585" : "#63BCA9",
+                },
+              ]}
             >
               사용 {isEmailDup ? "불" : ""}가능한 이메일 입니다.
             </Text>
@@ -153,14 +128,7 @@ export function StepOne({ control, setValue, errors }: StepOneProps) {
           )}
         />
 
-        <Text
-          style={{
-            fontSize: 10,
-            fontFamily: "Pretendard-SemiBold",
-            color: "#B7B7CB",
-            paddingHorizontal: 10,
-          }}
-        >
+        <Text style={styles.fieldIntroText}>
           *영문 대/소문자, 숫자, 기호(!@#$%^~*+=-) 사용하여 8~20 글자 사이의
           비밀번호를 사용하세요. 숫자와 특수기호가 한 글자 이상
           포함되어야합니다.
@@ -179,11 +147,12 @@ export function StepOne({ control, setValue, errors }: StepOneProps) {
           </Text>
           {isEmailDup !== null && (
             <Text
-              style={{
-                fontSize: 14,
-                fontFamily: "Pretendard-SemiBold",
-                color: isEmailDup ? "#FF8585" : "#63BCA9",
-              }}
+              style={[
+                styles.fieldAlertText,
+                {
+                  color: isEmailDup ? "#FF8585" : "#63BCA9",
+                },
+              ]}
             >
               사용 {isEmailDup ? "불" : ""}가능한 이메일 입니다.
             </Text>
@@ -209,38 +178,73 @@ export function StepOne({ control, setValue, errors }: StepOneProps) {
       {/**
        * 약관동의
        */}
-      <Controller
-        name="agreement"
-        control={control}
-        rules={{ required: true }}
-        render={() => (
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 9 }}>
-            <CheckBoxButton
-              isCheck={agreement}
-              onPress={agreementButtonClickHandler}
-            />
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                width: "100%",
-                flexGrow: 1,
-                flexShrink: 1,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontFamily: "Pretendard-SemiBold",
-                }}
-              >
-                서비스 이용 및 개인정보 수집약관에 동의합니다.
-              </Text>
-              <CaretRight />
-            </View>
-          </View>
-        )}
-      />
+      <View style={styles.agreementFieldControllerLayoutView}>
+        <CheckBoxButton
+          isCheck={agreement}
+          onPress={agreementButtonClickHandler}
+        />
+
+        <View style={styles.agreementFieldControllerTextLayoutView}>
+          <Text style={styles.fieldGuideText}>
+            서비스 이용 및 개인정보 수집약관에 동의합니다.
+          </Text>
+          <CaretRight />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 18,
+    paddingHorizontal: 20,
+    height: "100%",
+    width: "100%",
+  },
+
+  fieldView: {
+    gap: 12,
+  },
+
+  fieldTextLayoutView: {
+    flexDirection: "row",
+    gap: 12,
+  },
+
+  fieldGuideText: {
+    fontSize: 14,
+    fontFamily: "Pretendard-SemiBold",
+  },
+
+  fieldAlertText: {
+    fontSize: 14,
+    fontFamily: "Pretendard-SemiBold",
+  },
+
+  fieldIntroText: {
+    fontSize: 10,
+    fontFamily: "Pretendard-SemiBold",
+    color: "#B7B7CB",
+    paddingHorizontal: 10,
+  },
+
+  emailFieldControllerLayoutView: {
+    flexDirection: "row",
+    gap: 15,
+  },
+
+  agreementFieldControllerLayoutView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 9,
+  },
+
+  agreementFieldControllerTextLayoutView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
