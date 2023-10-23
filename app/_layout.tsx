873ad0fbf29b61119a455d4e32e7cb041fdf468f
junghyunbak <jeong5728@gmail.com>
2023-10-23 17:08:13 +0900

    feat: `App.tsx` 로직 이동 및 웹 스플래시 화면 구현

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { View, StyleSheet, Platform } from "react-native";
+import Constants from "expo-constants";
 
 export default function AppLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <View style={styles.container}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: Platform.OS === "ios" ? 20 : 0,
+    overflow: "hidden",
+  },
+});
