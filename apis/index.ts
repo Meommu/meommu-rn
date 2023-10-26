3210adbc25f1d8c2e6997bfaf634215877776df9
junghyunbak <jeong5728@gmail.com>
2023-10-26 17:18:22 +0900

    refactor: vercel.json API reverse proxy 설정

--- apis/index.ts
+++ apis/index.ts
@@ -1,13 +1,6 @@
 import * as GET from "./methods/get";
 import * as POST from "./methods/post";
 
-import axios from "axios";
-
-if (process.env.NODE_ENV !== "development") {
-  axios.defaults.baseURL =
-    "https://port-0-meommu-api-jvvy2blm5wku9j.sel5.cloudtype.app";
-}
-
 export const apiService = {
   ...GET,
   ...POST,
