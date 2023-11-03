991b71236c7940eff08a194421d2d2ed889ba5fb
junghyunbak <jeong5728@gmail.com>
2023-11-03 23:01:02 +0900

    fix: useToast에 debouncing 적용

--- hooks/useToast.ts
+++ hooks/useToast.ts
@@ -1,14 +1,24 @@
+// react
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
+
+// redux
 import { changeVisible, changeMessage } from "@/store/modules/toast";
 
 export function useToast() {
   const dispatch = useDispatch();
 
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+
   const fireToast = (message: string, duration: number) => {
     dispatch(changeMessage(message));
     dispatch(changeVisible(true));
 
-    setTimeout(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+
+    timer.current = setTimeout(() => {
       dispatch(changeVisible(false));
     }, duration);
   };
