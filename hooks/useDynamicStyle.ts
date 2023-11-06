07c5425f5f6163901e37b5ac7e4d35e9fe689f8b
junghyunbak <jeong5728@gmail.com>
2023-11-06 11:15:31 +0900

    feat: 스타일 동적지정 훅 구현

---
+++ hooks/useDynamicStyle.ts
@@ -0,0 +1,21 @@
+// react
+import { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import type { ViewStyle, ImageStyle, TextStyle } from "react-native";
+
+const createStyle = (style: ViewStyle | ImageStyle | TextStyle) => {
+  return StyleSheet.create({ _style: style })._style;
+};
+
+export const useDyanmicStyle = (
+  dynamicStyle: () => ViewStyle | ImageStyle | TextStyle,
+  deps?: React.DependencyList
+) => {
+  const [style, setStyle] = useState(createStyle(dynamicStyle()));
+
+  useEffect(() => {
+    setStyle(createStyle(dynamicStyle()));
+  }, deps);
+
+  return style;
+};
