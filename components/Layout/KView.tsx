e67c3a8b46b7c1c4a68bfeff8a4b6f6adfd9ee0e
junghyunbak <jeong5728@gmail.com>
2023-11-06 13:29:30 +0900

    fix: ios의 경우에만 KeyboardAvoidingView를 적용하도록 처리

--- components/Layout/KView.tsx
+++ components/Layout/KView.tsx
@@ -1,8 +1,10 @@
+// react
 import {
   KeyboardAvoidingView,
   Keyboard,
   TouchableWithoutFeedback,
   Platform,
+  View,
 } from "react-native";
 import type { KeyboardAvoidingViewProps } from "react-native";
 
@@ -12,14 +14,22 @@ interface KViewProps extends KeyboardAvoidingViewProps {}
  * input이 내부에 존재하는 화면을 구현할 때 사용하는 뷰
  */
 export function KView({ children, ...props }: KViewProps) {
+  if (Platform.OS === "ios") {
+    return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "position"}
+          {...props}
+        >
+          {children}
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    );
+  }
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "position"}
-        {...props}
-      >
-        {children}
-      </KeyboardAvoidingView>
+      <View {...props}>{children}</View>
     </TouchableWithoutFeedback>
   );
 }
