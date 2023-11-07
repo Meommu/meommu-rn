8b7725d7818fb6103ec4e782f3eb0d236c41d4bb
junghyunbak <jeong5728@gmail.com>
2023-11-07 17:40:48 +0900

    feat: 일기 조회 날짜 관련 전역 상태 추가

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
+
 import toast from "./modules/toast";
+import diaryDate from "./modules/diaryDate";
 
 const rootReducer = combineReducers({
   toast,
+  diaryDate,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ store/modules/diaryDate.ts
@@ -0,0 +1,57 @@
+const enum UPDATE_DIARY_DATE {
+  CHANGE_CURRENT_YEAR_MONTH = "CHANGE_CURRENT_YEAR_MONTH",
+  CHANGE_SELECTED_YEAR_MONTH = "CHANGE_SELECTED_YEAR_MONTH",
+}
+
+export const changeCurrentYearMonth = (year: number, month: number) => ({
+  type: UPDATE_DIARY_DATE.CHANGE_CURRENT_YEAR_MONTH,
+  year,
+  month,
+});
+
+export const changeSelectedYearMonth = (year: number, month: number) => ({
+  type: UPDATE_DIARY_DATE.CHANGE_SELECTED_YEAR_MONTH,
+  year,
+  month,
+});
+
+export interface DiaryDateState {
+  currentYear: number;
+  currentMonth: number;
+  selectedYear: number;
+  selectedMonth: number;
+}
+
+interface DiaryDateAction {
+  type: UPDATE_DIARY_DATE;
+  year: number;
+  month: number;
+}
+
+const initialState = {
+  currentYear: new Date().getFullYear(),
+  currentMonth: new Date().getMonth() + 1,
+  selectedYear: new Date().getFullYear(),
+  selectedMonth: new Date().getMonth() + 1,
+};
+
+const diaryDate = (
+  state: DiaryDateState = initialState,
+  action: DiaryDateAction
+): DiaryDateState => {
+  const { type, year, month } = action;
+
+  switch (type) {
+    case UPDATE_DIARY_DATE.CHANGE_CURRENT_YEAR_MONTH: {
+      return { ...state, currentYear: year, currentMonth: month };
+    }
+    case UPDATE_DIARY_DATE.CHANGE_SELECTED_YEAR_MONTH: {
+      return { ...state, selectedYear: year, selectedMonth: month };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default diaryDate;
