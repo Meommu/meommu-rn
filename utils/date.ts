1483512932833f0808ec3e3c342b57d2d7ae9cb5
junghyunbak <jeong5728@gmail.com>
2023-11-08 01:16:25 +0900

    feat: Date의 년, 월을 이용해 키 값을 생성하는 유틸함수 제작

--- utils/date.ts
+++ utils/date.ts
@@ -3,3 +3,11 @@ export const getPastYearDate = (yearCount: number) => {
 
   return new Date(now.setFullYear(now.getFullYear() - yearCount));
 };
+
+export const createYearMonthKey = (date: Date) => {
+  const year = date.getFullYear();
+
+  const month = date.getMonth() + 1;
+
+  return `${year}|${month}`;
+};
