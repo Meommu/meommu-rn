6a361d69d175e88a2c35a55365cb14b7cbac05aa
junghyunbak <jeong5728@gmail.com>
2023-11-08 16:22:43 +0900

    feat: 설정 페이지 헤더, sign 관련 버튼 구현

--- app/setting/index.tsx
+++ app/setting/index.tsx
@@ -1,5 +1,5 @@
 // react
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, Pressable } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // expo
@@ -7,6 +7,10 @@ import { router } from "expo-router";
 
 // constants
 import { VIEW_NAME } from "@/constants";
+
+// components
+import { GoBackButton } from "@/components/Button/GoBackButton";
+import { Header } from "@/components/Layout/Header";
 
 export default function Setting() {
   const handleLogoutButtonClick = () => {
@@ -16,10 +20,61 @@ export default function Setting() {
   };
 
   return (
-    <View>
-      <Text>세팅 화면</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Header title="설정" left={<GoBackButton />} />
 
-      <Button onPress={handleLogoutButtonClick} title="로그아웃" />
+        <View style={styles.sign}>
+          <Pressable
+            style={styles.signButton}
+            onPress={handleLogoutButtonClick}
+          >
+            <Text style={styles.signButtonText}>로그아웃</Text>
+          </Pressable>
+
+          <View style={styles.splitBar} />
+
+          <Pressable style={styles.signButton}>
+            <Text style={styles.signButtonText}>회원 탈퇴</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+
+  content: {
+    flex: 1,
+    margin: 20,
+    position: "relative",
+  },
+
+  sign: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+  },
+
+  signButton: {
+    width: "50%",
+  },
+
+  signButtonText: {
+    color: "#ABB0BA",
+    fontSize: 14,
+    fontFamily: "Pretendard-Regular",
+    textAlign: "center",
+  },
+
+  splitBar: {
+    borderWidth: 1,
+    borderColor: "#ABB0BA",
+  },
+});
