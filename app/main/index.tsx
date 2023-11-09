89b16dc8de90cca18bf7eb93aa4f3187626be828
junghyunbak <jeong5728@gmail.com>
2023-11-09 13:39:05 +0900

    refactor: `월` 선택 컴포넌트 분리

--- app/main/index.tsx
+++ app/main/index.tsx
@@ -1,174 +1,24 @@
 // react
-import React, { useRef, useMemo, useCallback, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  useWindowDimensions,
-  Platform,
-  Pressable,
-} from "react-native";
-import { useQuery } from "react-query";
-
-// redux
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "@/store";
-import type { DiaryDateState } from "@/store/modules/diaryDate";
-import {
-  applyCurrentBySelected,
-  changeCurrentYearMonth,
-  changeSelectedYearMonth,
-  updateDiariesThumbnailImage,
-} from "@/store/modules/diaryDate";
-
-// apis
-import { apiService } from "@/apis";
+import React, { Suspense } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 // expo
 import { router } from "expo-router";
 
 // constants
-import { VIEW_NAME, size } from "@/constants";
-
-// hooks
-import { useDyanmicStyle } from "@/hooks";
+import { VIEW_NAME } from "@/constants";
 
 // components
-import { SText } from "@/components/Text/SText";
-import { NavigationButton } from "@/components/Button/NavigationButton";
 import { PlusButton } from "@/components/Button/PlusButton";
-import { MonthPicker } from "@/components/MonthPicker";
 import { SettingButton } from "@/components/Button/SettingButton";
 import { DiaryList } from "@/components/DiaryList";
-
-// svgs
-import ArrowDropDown from "@/assets/svgs/arrow-drop-down.svg";
-
-// bottom sheets
-import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-  BottomSheetView,
-  useBottomSheetDynamicSnapPoints,
-  BottomSheetBackdrop,
-  BottomSheetBackdropProps,
-} from "@gorhom/bottom-sheet";
-
-// utils
-import { createYearMonthKey } from "@/utils";
 import { Header } from "@/components/Layout/Header";
+import {
+  MonthPickerOpenController,
+  MonthPickerOpenControllerSkeleton,
+} from "@/components/MonthPickerOpenController";
 
 export default function Main() {
-  /**
-   * bottomSheets
-   */
-  const bottomSheetRef = useRef<BottomSheetModal>(null);
-
-  const initialSnapPoints = useMemo(() => ["CONTENT_HEIGHT"], []);
-
-  const {
-    animatedHandleHeight,
-    animatedSnapPoints,
-    animatedContentHeight,
-    handleContentLayout,
-  } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
-
-  const handleSheetOpen = useCallback(() => {
-    bottomSheetRef.current?.present();
-  }, []);
-
-  /**
-   * diary date
-   */
-  const { data, isLoading } = useQuery(
-    [],
-    async () => {
-      return await apiService.getDiariesSummary();
-    },
-    {
-      retry: 0,
-    }
-  );
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (!data || !data.length) {
-      return;
-    }
-
-    /**
-     * 최신 일기가 존재하는 년, 월 추출
-     */
-    const latestDate = data
-      .map(({ date }) => new Date(date))
-      .sort((a, b) => {
-        return a > b ? -1 : 1;
-      })[0];
-
-    const year = latestDate.getFullYear();
-    const month = latestDate.getMonth() + 1;
-
-    dispatch(changeCurrentYearMonth(year, month));
-    dispatch(changeSelectedYearMonth(year, month));
-
-    /**
-     * 년, 월에 존재하는 일기들의 대표 이미지 추출
-     */
-    const yearMonthToImageId: Map<string, number> = new Map();
-
-    data.forEach(({ date, imageIds }) => {
-      if (!imageIds.length) {
-        return;
-      }
-
-      yearMonthToImageId.set(createYearMonthKey(new Date(date)), imageIds[0]);
-    });
-
-    dispatch(updateDiariesThumbnailImage(yearMonthToImageId));
-  }, [data]);
-
-  const { currentMonth, currentYear } = useSelector<RootState, DiaryDateState>(
-    (state) => state.diaryDate
-  );
-
-  const handleDatePickButtonClick = () => {
-    dispatch(applyCurrentBySelected());
-
-    bottomSheetRef.current?.dismiss();
-  };
-
-  /**
-   * bottom sheet의 반응형 너비 계산
-   */
-  const { width, height } = useWindowDimensions();
-
-  const bottomSheetMaxWidthStyle = useDyanmicStyle(() => {
-    const maxWidth =
-      Platform.OS === "web" && width >= size.LAPTOP_WIDTH
-        ? (9 * height) / 16
-        : "100%";
-
-    return { maxWidth };
-  }, [width, height]);
-
-  /**
-   * bottom sheet의 dimmed
-   *
-   * ※ Web 환경에서는 뒷 배경 클릭시 바텀시트 모달이 닫히지 않음.
-   * ※ v5 버전에서 업데이트를 기다려야 할 것 같음.
-   */
-  const renderBackdrop = useCallback(
-    (props: BottomSheetBackdropProps) => (
-      <BottomSheetBackdrop
-        {...props}
-        appearsOnIndex={0}
-        disappearsOnIndex={-1}
-      />
-    ),
-    []
-  );
-
   /**
    * 컨트롤러(세팅, 글쓰기) 버튼 핸들러
    */
@@ -177,68 +27,25 @@ export default function Main() {
   };
 
   return (
-    <BottomSheetModalProvider>
-      <View style={styles.container}>
-        {/**
-         * 헤더
-         */}
-        <View style={styles.headerWrapper}>
-          <Header
-            left={<Text style={styles.logoText}>meommu</Text>}
-            right={
-              <View style={styles.controllerBox}>
-                <SettingButton onPress={handleSettingButtonClick} />
-                <PlusButton />
-              </View>
-            }
-          />
-        </View>
-
-        {/**
-         * 날짜 선택기
-         */}
-        {isLoading ? (
-          <View style={styles.datePicker}>
-            <SText textLength={7} />
-          </View>
-        ) : (
-          <Pressable style={styles.datePicker} onPress={handleSheetOpen}>
-            <Text style={styles.datePickerText}>
-              {currentYear}년 {currentMonth}월
-            </Text>
-            <ArrowDropDown />
-          </Pressable>
-        )}
-
-        <DiaryList />
-
-        {/**
-         * 날짜 선택기 바텀시트 모달
-         */}
-        <BottomSheetModal
-          ref={bottomSheetRef}
-          snapPoints={animatedSnapPoints}
-          contentHeight={animatedContentHeight}
-          handleHeight={animatedHandleHeight}
-          enableContentPanningGesture={false}
-          containerStyle={[
-            bottomSheetMaxWidthStyle,
-            styles.bottomSheetContainer,
-          ]}
-          handleIndicatorStyle={styles.handleIndicator}
-          backdropComponent={renderBackdrop}
-        >
-          <BottomSheetView onLayout={handleContentLayout}>
-            <MonthPicker />
-            <NavigationButton
-              content="확인"
-              style={styles.chooseMonthButton}
-              onPress={handleDatePickButtonClick}
-            />
-          </BottomSheetView>
-        </BottomSheetModal>
+    <View style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Header
+          left={<Text style={styles.logoText}>meommu</Text>}
+          right={
+            <View style={styles.controllerBox}>
+              <SettingButton onPress={handleSettingButtonClick} />
+              <PlusButton />
+            </View>
+          }
+        />
       </View>
-    </BottomSheetModalProvider>
+
+      <Suspense fallback={<MonthPickerOpenControllerSkeleton />}>
+        <MonthPickerOpenController />
+      </Suspense>
+
+      <DiaryList />
+    </View>
   );
 }
 
@@ -268,37 +75,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingVertical: 8,
     gap: 16,
-  },
-
-  /**
-   * date picker
-   */
-  datePicker: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    paddingBottom: 20,
-    gap: 12,
-  },
-
-  datePickerText: {
-    fontFamily: "yeonTheLand",
-    color: "#89899C",
-  },
-
-  /**
-   * bottom sheet
-   */
-  bottomSheetContainer: {
-    marginHorizontal: "auto",
-  },
-
-  handleIndicator: {
-    backgroundColor: "rgba(0, 0, 0, 0.3)",
-    width: "10%",
-  },
-
-  chooseMonthButton: {
-    padding: 20,
   },
 });
---
+++ components/MonthPickerOpenController.tsx
@@ -0,0 +1,238 @@
+// react
+import { useEffect, useMemo, useRef, useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Platform,
+  useWindowDimensions,
+} from "react-native";
+import { useQuery } from "react-query";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "@/store";
+import type { DiaryDateState } from "@/store/modules/diaryDate";
+import {
+  applyCurrentBySelected,
+  changeCurrentYearMonth,
+  changeSelectedYearMonth,
+  updateDiariesThumbnailImage,
+} from "@/store/modules/diaryDate";
+
+// components
+import { SText } from "./Text/SText";
+import { MonthPicker } from "@/components/MonthPicker";
+import { NavigationButton } from "@/components/Button/NavigationButton";
+
+// hooks
+import { useDyanmicStyle } from "@/hooks";
+
+// svgs
+import ArrowDropDown from "@/assets/svgs/arrow-drop-down.svg";
+
+// constants
+import { size } from "@/constants";
+
+// apis
+import { apiService } from "@/apis";
+
+// utils
+import { createYearMonthKey } from "@/utils";
+
+// bottom sheets
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+  useBottomSheetDynamicSnapPoints,
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+} from "@gorhom/bottom-sheet";
+
+export function MonthPickerOpenController() {
+  const dispatch = useDispatch();
+
+  const { currentMonth, currentYear } = useSelector<RootState, DiaryDateState>(
+    (state) => state.diaryDate
+  );
+
+  /**
+   * diary date
+   */
+  const { data } = useQuery(
+    [],
+    async () => {
+      return await apiService.getDiariesSummary();
+    },
+    {
+      retry: 0,
+      suspense: true,
+    }
+  );
+
+  useEffect(() => {
+    if (!data || !data.length) {
+      return;
+    }
+
+    /**
+     * 최신 일기가 존재하는 년, 월 추출
+     */
+    const latestDate = data
+      .map(({ date }) => new Date(date))
+      .sort((a, b) => {
+        return a > b ? -1 : 1;
+      })[0];
+
+    const year = latestDate.getFullYear();
+    const month = latestDate.getMonth() + 1;
+
+    dispatch(changeCurrentYearMonth(year, month));
+    dispatch(changeSelectedYearMonth(year, month));
+
+    /**
+     * 년, 월에 존재하는 일기들의 대표 이미지 추출
+     */
+    const yearMonthToImageId: Map<string, number> = new Map();
+
+    data.forEach(({ date, imageIds }) => {
+      if (!imageIds.length) {
+        return;
+      }
+
+      yearMonthToImageId.set(createYearMonthKey(new Date(date)), imageIds[0]);
+    });
+
+    dispatch(updateDiariesThumbnailImage(yearMonthToImageId));
+  }, [data]);
+
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
+  const initialSnapPoints = useMemo(() => ["CONTENT_HEIGHT"], []);
+
+  const {
+    animatedHandleHeight,
+    animatedSnapPoints,
+    animatedContentHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
+
+  const handleDatePickButtonClick = () => {
+    dispatch(applyCurrentBySelected());
+
+    bottomSheetRef.current?.dismiss();
+  };
+
+  const handleSheetOpen = useCallback(() => {
+    bottomSheetRef.current?.present();
+  }, []);
+
+  /**
+   * bottom sheet의 반응형 너비 계산
+   */
+  const { width, height } = useWindowDimensions();
+
+  const bottomSheetMaxWidthStyle = useDyanmicStyle(() => {
+    const maxWidth =
+      Platform.OS === "web" && width >= size.LAPTOP_WIDTH
+        ? (9 * height) / 16
+        : "100%";
+
+    return { maxWidth };
+  }, [width, height]);
+
+  /**
+   * bottom sheet의 dimmed
+   *
+   * ※ Web 환경에서는 뒷 배경 클릭시 바텀시트 모달이 닫히지 않음.
+   * ※ v5 버전에서 업데이트를 기다려야 할 것 같음.
+   */
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...props}
+        style={[props.style, styles.bottomSheetBackdrop]}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+
+  return (
+    <BottomSheetModalProvider>
+      <Pressable style={styles.monthPicker} onPress={handleSheetOpen}>
+        <Text style={styles.monthPickerText}>
+          {currentYear}년 {currentMonth}월
+        </Text>
+        <ArrowDropDown />
+      </Pressable>
+
+      <BottomSheetModal
+        ref={bottomSheetRef}
+        snapPoints={animatedSnapPoints}
+        contentHeight={animatedContentHeight}
+        handleHeight={animatedHandleHeight}
+        enableContentPanningGesture={false}
+        containerStyle={[bottomSheetMaxWidthStyle, styles.bottomSheetContainer]}
+        handleIndicatorStyle={styles.handleIndicator}
+        backdropComponent={renderBackdrop}
+      >
+        <BottomSheetView onLayout={handleContentLayout}>
+          <MonthPicker />
+          <NavigationButton
+            content="확인"
+            style={styles.chooseMonthButton}
+            onPress={handleDatePickButtonClick}
+          />
+        </BottomSheetView>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
+  );
+}
+
+export function MonthPickerOpenControllerSkeleton() {
+  return (
+    <View style={styles.monthPicker}>
+      <SText style={styles.monthPickerText} textLength={7} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  monthPicker: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    gap: 12,
+  },
+
+  monthPickerText: {
+    fontFamily: "yeonTheLand",
+    color: "#89899C",
+  },
+
+  /**
+   * bottom sheet
+   */
+  bottomSheetBackdrop: {
+    zIndex: 999,
+  },
+
+  bottomSheetContainer: {
+    marginHorizontal: "auto",
+    zIndex: 1000,
+  },
+
+  handleIndicator: {
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    width: "10%",
+  },
+
+  chooseMonthButton: {
+    padding: 20,
+  },
+});
