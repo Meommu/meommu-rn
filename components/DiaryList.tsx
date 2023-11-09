16b5f567c976f8e7f35668407bbd8e0792cccc80
junghyunbak <jeong5728@gmail.com>
2023-11-09 14:06:06 +0900

    refactor: 리스트 placeholder 컴포넌트 분리 및 리스트 노출 로직 수정

--- components/DiaryList.tsx
+++ components/DiaryList.tsx
@@ -13,6 +13,7 @@ import { apiService } from "@/apis";
 // components
 import { DiaryItem, DiaryItemSkeleton } from "./DiaryItem";
 import { NonIndicatorScrollView } from "./ScrollView/NonIndicatorScrollView";
+import { DiaryListPlaceholder } from "./DiaryListPlaceholder";
 
 export function DiaryList() {
   const { currentMonth, currentYear } = useSelector<RootState, DiaryDateState>(
@@ -26,59 +27,25 @@ export function DiaryList() {
     }
   );
 
-  /**
-   * TODO: 무한 스크롤 적용하여 한번에 가져올 수 있는 개수만큼 스켈레톤 ui 렌더링하기
-   */
-  if (isLoading) {
-    return (
-      <NonIndicatorScrollView style={styles.container}>
-        {Array(3)
+  const diaries = data || [];
+
+  return (
+    <NonIndicatorScrollView style={styles.container}>
+      {diaries.map((diary) => {
+        return <DiaryItem diary={diary} key={diary.id} />;
+      })}
+
+      {isLoading &&
+        Array(3)
           .fill(null)
           .map((_, i) => {
             return <DiaryItemSkeleton key={i} />;
           })}
-      </NonIndicatorScrollView>
-    );
-  }
 
-  if (!data || !data.length) {
-    return (
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 10,
-        }}
-      >
-        <Image
-          style={{ width: 160, height: 160 }}
-          source={require("@/assets/images/main/placeholder.png")}
-        />
-        <Text
-          style={{
-            fontFamily: "yeonTheLand",
-            fontSize: 20,
-            color: "#89899C",
-          }}
-        >
-          안녕하세요! 멈무에요{"\n"}일기를 등록해주세요
-        </Text>
-      </View>
-    );
-  }
-
-  return (
-    <NonIndicatorScrollView style={styles.container}>
-      {data && (
-        <View>
-          {data.map((diary) => {
-            return <DiaryItem diary={diary} key={diary.id} />;
-          })}
-
-          <Text style={styles.listCountText}>{data.length}개의 일기</Text>
-        </View>
+      {diaries.length > 0 ? (
+        <Text style={styles.listCountText}>{diaries.length}개의 일기</Text>
+      ) : (
+        <DiaryListPlaceholder />
       )}
     </NonIndicatorScrollView>
   );
