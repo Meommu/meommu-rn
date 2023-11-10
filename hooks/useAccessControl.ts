d691206f0341651ceccf35272ab3c0a8625ba17a
junghyunbak <jeong5728@gmail.com>
2023-11-10 19:19:52 +0900

    refactor: 로그아웃 되어있을 경우 루트 페이지로 튕겨내는 훅 구현

--- hooks/useAccessControl.ts
+++ hooks/useAccessControl.ts
@@ -13,6 +13,22 @@ import { useToast } from "./useToast";
 // constants
 import { VIEW_NAME } from "@/constants";
 
+export function useThrowRootIfLogout() {
+  const { fireToast } = useToast();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await apiService.getLoginInfo();
+      } catch (e) {
+        fireToast("잘못된 접근입니다.", 2000);
+
+        router.replace(VIEW_NAME.ROOT);
+      }
+    })();
+  }, []);
+}
+
 export function useThrowRootIfLogin() {
   const { fireToast } = useToast();
 
@@ -21,16 +37,10 @@ export function useThrowRootIfLogin() {
       try {
         await apiService.getLoginInfo();
 
-        /**
-         * 에러가 발생하지 않으면 로그인이 되어 있는 상태이므로
-         * 경고 메세지와 함께 루트 페이지로 라우트
-         */
         fireToast("잘못된 접근입니다.", 2000);
 
         router.replace(VIEW_NAME.ROOT);
-      } catch (e) {
-        // 로그인이 되어어있지 않은 상태이므로 정상적으로 이용
-      }
+      } catch (e) {}
     })();
   }, []);
 }
