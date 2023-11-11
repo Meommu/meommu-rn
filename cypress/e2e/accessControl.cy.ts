624ed5ca852cfadc865cf8a403195b65d1407ebe
junghyunbak <jeong5728@gmail.com>
2023-11-11 16:20:45 +0900

    test: 회원가입 페이지에 대한 접근제한 테스트코드 추가

--- cypress/e2e/accessControl.cy.ts
+++ cypress/e2e/accessControl.cy.ts
@@ -36,6 +36,14 @@ describe("로그인/비로그인 상태에서의 페이지 접근제어", () => 
     cy.contains(/[0-9]{4}년 [0-9]{1,2}월/, { timeout: 1000 * 10 });
   });
 
+  it("로그인 상태로 회원가입 페이지에 접근할 경우 메인 페이지로 이동 후 '잘못된 접근입니다.' 경고 메세지 출력", () => {
+    cy.visit("http://localhost:8081/sign-up");
+
+    cy.contains("잘못된 접근입니다.");
+
+    cy.contains(/[0-9]{4}년 [0-9]{1,2}월/, { timeout: 1000 * 10 });
+  });
+
   it("올바르게 로그아웃 할 경우 홈 페이지로 이동", () => {
     cy.get('[data-testid="button-setting"]').click();
 
