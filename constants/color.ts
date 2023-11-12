1a724028e7d4ae173c756397fe14e1bfd12cfec5
junghyunbak <jeong5728@gmail.com>
2023-11-12 15:29:48 +0900

    feat: 컬러 팔레트 상수 추가

---
+++ constants/color.ts
@@ -0,0 +1,3 @@
+export const primary = "#8579F1";
+
+export const inactive = "#B7B7CB";
--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,4 @@ export * from "./viewName";
 export * from "./code";
 export * as size from "./size";
 export * as zIndex from "./zIndex";
+export * as color from "./color";
