5b977b96b06fdbb10b4db4ed4dc2266b3b781071
junghyunbak <jeong5728@gmail.com>
2023-11-14 00:04:12 +0900

    feat: 글쓰기 페이지에 키보드 고려한 레이아웃 적용

--- app/write/index.tsx
+++ app/write/index.tsx
@@ -1,8 +1,10 @@
 // react
+import { StyleSheet } from "react-native";
 import { FormProvider, useForm } from "react-hook-form";
 
 // components
 import { WritePresenter } from "@/components/WritePresenter";
+import { KView } from "@/components/Layout/KView";
 
 export default function Write() {
   const methods = useForm<DiaryWriteFormFieldValues>({
@@ -16,8 +18,17 @@ export default function Write() {
   });
 
   return (
-    <FormProvider {...methods}>
-      <WritePresenter />
-    </FormProvider>
+    <KView style={styles.container}>
+      <FormProvider {...methods}>
+        <WritePresenter />
+      </FormProvider>
+    </KView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+});
