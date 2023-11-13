a29e4417e77b9f48e01f00f6e0bf110d510fbb15
junghyunbak <jeong5728@gmail.com>
2023-11-14 00:43:56 +0900

    style: KView 웹 환경에서 커서 normal이 되도록 수정

--- components/Layout/KView.tsx
+++ components/Layout/KView.tsx
@@ -39,5 +39,10 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
+    ...Platform.select({
+      web: {
+        cursor: "normal",
+      },
+    }),
   },
 });
