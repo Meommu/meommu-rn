2933af1c54f3a92df06924306837e77abbb9e21a
junghyunbak <jeong5728@gmail.com>
2023-11-13 22:33:09 +0900

    refactor: plusbutton 컴포넌트 스타일 분리 및 구조 개선

--- components/Button/PlusButton.tsx
+++ components/Button/PlusButton.tsx
@@ -1,5 +1,6 @@
 // react
-import { Pressable, PressableProps } from "react-native";
+import { View, Pressable, StyleSheet } from "react-native";
+import type { PressableProps } from "react-native";
 
 // svgs
 import Plus from "@/assets/svgs/plus.svg";
@@ -8,8 +9,24 @@ interface PlusButtonProps extends PressableProps {}
 
 export function PlusButton({ ...props }: PlusButtonProps) {
   return (
-    <Pressable {...props}>
-      <Plus />
-    </Pressable>
+    <View style={styles.container}>
+      <Pressable style={styles.hitBox} {...props}>
+        <Plus />
+      </Pressable>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 20,
+    height: 20,
+  },
+
+  hitBox: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
