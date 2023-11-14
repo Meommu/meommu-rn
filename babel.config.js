e616c09b91ea9c002ab9912535a1f2cce03a1bbf
junghyunbak <jeong5728@gmail.com>
2023-11-14 16:58:39 +0900

    chore: expo console.log 미동작 이유 주석으로 작성 (추측)

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,11 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       "expo-router/babel",
-      // https://docs.expo.dev/versions/latest/sdk/reanimated/#web-support
+      /**
+       * 아래 플러그인 추가 이유 : https://docs.expo.dev/versions/latest/sdk/reanimated/#web-support
+       *
+       * side effect : https://github.com/facebook/metro/issues/877
+       */
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
     ],
