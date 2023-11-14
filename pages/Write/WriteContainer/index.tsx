10627f6767839b4eb8597d9d3fe80155648197c5
junghyunbak <jeong5728@gmail.com>
2023-11-15 04:37:07 +0900

    refactor: useSwiper 적용, 주석 추가

--- pages/Write/WriteContainer/index.tsx
+++ pages/Write/WriteContainer/index.tsx
@@ -1,6 +1,5 @@
 // react
-import { useState, useRef, useCallback } from "react";
-import Swiper from "react-native-web-swiper";
+import { useCallback } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 
 // expo
@@ -9,6 +8,9 @@ import { router } from "expo-router";
 // components
 import { WritePresenter } from "../WritePresenter";
 
+// hooks
+import { useSwiper } from "@/hooks/useSwiper";
+
 // constants
 import { PATH } from "@/constants";
 
@@ -16,6 +18,9 @@ const FIRST_SLIDE_INDEX = 0;
 const LAST_SLIDE_INDEX = 1;
 
 export function WriteContainer() {
+  /**
+   * useForm
+   */
   const methods = useForm<DiaryWriteFormFieldValues>({
     defaultValues: {
       date: "",
@@ -28,14 +33,15 @@ export function WriteContainer() {
 
   const { handleSubmit, watch } = methods;
 
-  const [swiperIndex, setSwiperIndex] = useState(0);
+  /**
+   * swiper
+   */
+  const { swiperIndex, swiperRef, handleSwiperIndexChange } =
+    useSwiper(FIRST_SLIDE_INDEX);
 
-  const swiperRef = useRef<Swiper | null>(null);
-
-  const handleSwiperIndexChange = useCallback((index: number) => {
-    setSwiperIndex(index);
-  }, []);
-
+  /**
+   * event handlers
+   */
   const handleBottomButtonClick = useCallback(() => {
     switch (swiperIndex) {
       case FIRST_SLIDE_INDEX:
@@ -70,6 +76,9 @@ export function WriteContainer() {
     }
   }, [swiperIndex, swiperRef]);
 
+  /**
+   * util functions
+   */
   const formState = watch();
 
   const isBottomButtonActive = useCallback(() => {
