cb6bcce2f24b4efeb64eb9e84f77a52828f87f0d
junghyunbak <jeong5728@gmail.com>
2023-11-15 16:12:41 +0900

    chore: 리팩토링으로 인해 사용되지 않는 컴포넌트 & 전역 상태 삭제

--- store/modules/diaryDate.ts
+++ store/modules/diaryDate.ts
@@ -1,15 +1,6 @@
 const enum UPDATE_DIARY_DATE {
-  CHANGE_CURRENT_YEAR_MONTH = "CHANGE_CURRENT_YEAR_MONTH",
   CHANGE_SELECTED_YEAR_MONTH = "CHANGE_SELECTED_YEAR_MONTH",
-  APPLY_CURRENT_DATE_BY_SELECTED = "APPLY_CURRENT_DATE_BY_SELECTED",
-  UPDATE_DIARIES_THUMBNAIL_IMAGE = "UPDATE_DIARIES_THUMBNAIL_IMAGE",
 }
-
-export const changeCurrentYearMonth = (year: number, month: number) => ({
-  type: UPDATE_DIARY_DATE.CHANGE_CURRENT_YEAR_MONTH,
-  year,
-  month,
-});
 
 export const changeSelectedYearMonth = (year: number, month: number) => ({
   type: UPDATE_DIARY_DATE.CHANGE_SELECTED_YEAR_MONTH,
@@ -17,64 +8,31 @@ export const changeSelectedYearMonth = (year: number, month: number) => ({
   month,
 });
 
-export const applyCurrentBySelected = () => ({
-  type: UPDATE_DIARY_DATE.APPLY_CURRENT_DATE_BY_SELECTED,
-});
-
-export const updateDiariesThumbnailImage = (
-  yearMonthToImageId: Map<string, number>
-) => ({
-  type: UPDATE_DIARY_DATE.UPDATE_DIARIES_THUMBNAIL_IMAGE,
-  yearMonthToImageId,
-});
-
 export interface DiaryDateState {
-  currentYear: number;
-  currentMonth: number;
   selectedYear: number;
   selectedMonth: number;
-  yearMonthToImageId: Map<string, number>;
 }
 
 interface DiaryDateAction {
   type: UPDATE_DIARY_DATE;
   year: number;
   month: number;
-  yearMonthToImageId: Map<string, number>;
 }
 
 const initialState = {
-  currentYear: new Date().getFullYear(),
-  currentMonth: new Date().getMonth() + 1,
   selectedYear: new Date().getFullYear(),
   selectedMonth: new Date().getMonth() + 1,
-  yearMonthToImageId: new Map(),
 };
 
 const diaryDate = (
   state: DiaryDateState = initialState,
   action: DiaryDateAction
 ): DiaryDateState => {
-  const { type, year, month, yearMonthToImageId } = action;
+  const { type, year, month } = action;
 
   switch (type) {
-    case UPDATE_DIARY_DATE.CHANGE_CURRENT_YEAR_MONTH: {
-      return { ...state, currentYear: year, currentMonth: month };
-    }
     case UPDATE_DIARY_DATE.CHANGE_SELECTED_YEAR_MONTH: {
       return { ...state, selectedYear: year, selectedMonth: month };
-    }
-    case UPDATE_DIARY_DATE.APPLY_CURRENT_DATE_BY_SELECTED: {
-      const { selectedMonth, selectedYear } = state;
-
-      return {
-        ...state,
-        currentYear: selectedYear,
-        currentMonth: selectedMonth,
-      };
-    }
-    case UPDATE_DIARY_DATE.UPDATE_DIARIES_THUMBNAIL_IMAGE: {
-      return { ...state, yearMonthToImageId };
     }
     default: {
       return state;
