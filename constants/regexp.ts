73808d6a04860b3f1a8e6adf904ee55a877553ac
junghyunbak <jeong5728@gmail.com>
2023-11-15 18:51:55 +0900

    chore: datetime 정규식 수정

--- constants/regexp.ts
+++ constants/regexp.ts
@@ -5,4 +5,4 @@ export const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 export const phone = /^\d{3}-\d{3,4}-\d{4}$/;
 
-export const date = /^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/;
+export const date = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
