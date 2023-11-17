6781b5c65193d87e4161966b251f43c52fad1764
junghyunbak <jeong5728@gmail.com>
2023-11-17 15:10:40 +0900

    feat: 이미지를 포함한 html를 캔버스로 변경하여 반환하는 api 함수 구현

--- apis/methods/get.ts
+++ apis/methods/get.ts
@@ -1,5 +1,9 @@
 // axios
 import axios from "axios";
+import { baseUrl } from "..";
+
+// html2canvas
+import html2canvas from "html2canvas";
 
 export const getEmailDuplicationStatus = async (
   email: string
@@ -65,4 +69,18 @@ export const getImageUrl = async (imageId: number): Promise<DiaryImage> => {
   );
 
   return data;
+};
+
+/**
+ * 항상 내부적으로 api를 요청하지는 않지만, html에 이미지가 존재하는 경우 api 요청이 이루어지므로
+ * api 주소를 한 곳에서 집중적으로 관리할 필요가 있어 해당 파일 다른 api 함수와 같은 형태로 작성함.
+ */
+export const getCanvasWithHtmlWithImage = async (
+  element: HTMLElement
+): Promise<HTMLCanvasElement> => {
+  const canvas = await html2canvas(element, {
+    proxy: `${baseUrl}/html2canvas/proxy.json`,
+  });
+
+  return canvas;
 };
