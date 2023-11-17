449a2e5ab7c8209e928583e5f244ac9c524ea33e
junghyunbak <jeong5728@gmail.com>
2023-11-17 14:53:47 +0900

    feat: 일기 캡처를 위한 View Ref 받을 수 있도록 인터페이스 수정

--- pages/Diary/DiaryPresenter/index.tsx
+++ pages/Diary/DiaryPresenter/index.tsx
@@ -10,9 +10,11 @@ import { ImageSlider } from "@/components/Widget/ImageSlider";
 
 import { NonIndicatorScrollView } from "@/components/ScrollView/NonIndicatorScrollView";
 import { NavigationButton } from "@/components/Button/NavigationButton";
+import React from "react";
 
 interface DiaryPresenterProps {
   diary: Diary;
+  imageRef?: React.MutableRefObject<View | null>;
   handleGoBackButtonClick: () => void;
   handleEditButtonClick: () => void;
   handleShareButtonClick: () => void;
@@ -20,6 +22,7 @@ interface DiaryPresenterProps {
 
 export function DiaryPresenter({
   diary,
+  imageRef,
   handleEditButtonClick,
   handleGoBackButtonClick,
   handleShareButtonClick,
@@ -33,44 +36,48 @@ export function DiaryPresenter({
       />
 
       <NonIndicatorScrollView>
-        <ImageSlider
-          imageIds={diary.imageIds}
-          aspectRatio="3/4"
-          borderRadius={0}
-        />
+        <View style={{ backgroundColor: "white" }} ref={imageRef}>
+          <ImageSlider
+            imageIds={diary.imageIds}
+            aspectRatio="3/4"
+            borderRadius={0}
+          />
 
-        <View style={{ paddingHorizontal: 20, paddingVertical: 33, gap: 16 }}>
-          <Text
-            style={{
-              color: "#626154",
-              fontSize: 26,
-              fontFamily: "yeonTheLand",
-            }}
-          >
-            {diary.title}
-          </Text>
+          <View style={{ paddingHorizontal: 20, paddingVertical: 33, gap: 16 }}>
+            <Text
+              style={{
+                color: "#626154",
+                fontSize: 26,
+                fontFamily: "yeonTheLand",
+              }}
+            >
+              {diary.title}
+            </Text>
 
-          <Text
-            style={{
-              color: "#626154",
-              fontSize: 17,
-              fontFamily: "yeonTheLand",
-            }}
-          >
-            {diary.content}
-          </Text>
+            <Text
+              style={{
+                color: "#626154",
+                fontSize: 17,
+                fontFamily: "yeonTheLand",
+              }}
+            >
+              {diary.content}
+            </Text>
 
-          <Text
-            style={{
-              color: "#8F8F8F",
-              fontSize: 14,
+            <Text
+              style={{
+                color: "#8F8F8F",
+                fontSize: 14,
 
-              fontFamily: "Pretendard-Regular",
-            }}
-          >
-            {diary.date.replaceAll("-", ".")} {diary.dogName} 일기
-          </Text>
+                fontFamily: "Pretendard-Regular",
+              }}
+            >
+              {diary.date.replaceAll("-", ".")} {diary.dogName} 일기
+            </Text>
+          </View>
+        </View>
 
+        <View style={{ padding: 20 }}>
           <NavigationButton onPress={handleShareButtonClick} content="보내기" />
         </View>
       </NonIndicatorScrollView>
