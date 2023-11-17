a2f950ab6f05dac54170d3fad6e44fcc48138a74
junghyunbak <jeong5728@gmail.com>
2023-11-17 15:56:10 +0900

    feat: 스켈레톤 뷰에 밝기 애니메이션 추가

--- components/Layout/SView.tsx
+++ components/Layout/SView.tsx
@@ -1,14 +1,55 @@
 // react
-import { View } from "react-native";
+import { useEffect, useRef } from "react";
 import type { ViewProps } from "react-native";
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import Animated from "react-native-reanimated";
 
-interface SViewProps extends ViewProps {}
+const BRIGHT = 0.3;
+const DARK = 0.8;
+
+interface SViewProps extends ViewProps {
+  duration?: number;
+}
 
 /**
  * SView: Skeleton View (스켈레톤 뷰)
  */
-export function SView({ style }: SViewProps) {
+export function SView({ style, duration = 1000 }: SViewProps) {
+  const interval = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const opacity = useSharedValue(DARK);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(opacity.value, { duration }),
+    };
+  });
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      opacity.value = opacity.value < 0.5 ? DARK : BRIGHT;
+    }, duration);
+
+    return () => {
+      if (!interval.current) {
+        return;
+      }
+
+      clearInterval(interval.current);
+    };
+  }, []);
+
   return (
-    <View style={[style, { backgroundColor: "lightgray", borderRadius: 10 }]} />
+    <Animated.View
+      style={[
+        animatedStyle,
+        style,
+        { backgroundColor: "lightgray", borderRadius: 4 },
+      ]}
+    />
   );
 }
