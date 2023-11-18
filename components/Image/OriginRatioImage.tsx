9c819a50969b23ad88d698c1c20c2f35972df6dd
junghyunbak <jeong5728@gmail.com>
2023-11-19 01:20:00 +0900

    feat: 비율을 유지하는 Image 컴포넌트 구현

---
+++ components/Image/OriginRatioImage.tsx
@@ -0,0 +1,76 @@
+// react
+import { useEffect, useState } from "react";
+import { Image } from "react-native";
+import type { ImageProps } from "react-native";
+
+// hooks
+import { useDynamicStyle } from "@/hooks";
+
+interface OriginRatioImageProps extends ImageProps {
+  imageUriForCalculateRatio: string;
+  expansion?: "width" | "height";
+}
+
+export function OriginRatioImage({
+  imageUriForCalculateRatio,
+  expansion,
+  style,
+  ...props
+}: OriginRatioImageProps) {
+  const [imageRatio, setImageRatio] = useState<string | null>(null);
+
+  useEffect(() => {
+    Image.getSize(imageUriForCalculateRatio, (width, height) => {
+      switch (expansion) {
+        case "width":
+          setImageRatio(`1/${height / width}`);
+          break;
+
+        case "height":
+          setImageRatio(`${width / height}/1`);
+          break;
+
+        default:
+          setImageRatio(`${width}/${height}`);
+          break;
+      }
+    });
+  }, []);
+
+  const imageRatioStyle = useDynamicStyle(() => {
+    if (!imageRatio) {
+      return {};
+    }
+
+    switch (expansion) {
+      case "width":
+        return {
+          width: "100%",
+          aspectRatio: imageRatio,
+        };
+
+      case "height":
+        return {
+          height: "100%",
+          aspectRatio: imageRatio,
+        };
+
+      default:
+        return {
+          aspectRatio: imageRatio,
+        };
+    }
+  }, [imageRatio]);
+
+  if (!imageRatio) {
+    return;
+  }
+
+  return (
+    <Image
+      {...props}
+      // @ts-ignore
+      style={[style, imageRatioStyle]}
+    />
+  );
+}
