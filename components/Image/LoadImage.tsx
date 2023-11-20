cea0d9178756a19fb245fa21fef8903b357aabe2
junghyunbak <jeong5728@gmail.com>
2023-11-20 14:56:28 +0900

    refactor: LoadImage 원본비율 옵션 컴포넌트 재활용하여 구현

--- components/Image/LoadImage.tsx
+++ components/Image/LoadImage.tsx
@@ -1,32 +1,43 @@
 // react
 import { View, Image } from "react-native";
-import type { ImageStyle, ViewProps } from "react-native";
+import type { ViewProps, ImageStyle } from "react-native";
 import { useQuery } from "react-query";
-import { useState } from "react";
 
 // apis
 import { apiService } from "@/apis";
 
-// hooks
-import { useDynamicStyle } from "@/hooks";
-
-global.Buffer = global.Buffer || require("buffer").Buffer;
+// components
+import { OriginRatioImage } from "./OriginRatioImage";
 
 interface LoadImageProps extends ViewProps {
   imageId: number;
-  original?: boolean;
+
+  /**
+   * ImageProps를 컴포넌트 인터페이스로 설정할 경우 source 값을 필수로 입력받게 되므로,
+   * ViewProps를 사용하되 style만 ImageStyle 타입으로 전달받도록 함
+   */
+  style?: ImageStyle;
+
   /**
    * 화면의 너비는 꽉 채우되, 실제 이미지 비율을 유지하는 이미지 요소를 얻고 싶을 때 사용
    *
    * default: false
    */
   originRatio?: boolean;
+
+  /**
+   * originRatio가 true일 때, 가로 세로 중 어느 요소를 기준으로 이미지를 확장시킬지를 정하는 옵션
+   *
+   * default: undefined
+   */
+  expansion?: "width" | "height";
 }
 
 export function LoadImage({
   imageId,
   style,
   originRatio = false,
+  expansion,
 }: LoadImageProps) {
   const { data } = useQuery(["diaryImage", imageId], async () => {
     const { url } = await apiService.getImageUrl(imageId);
@@ -34,29 +45,17 @@ export function LoadImage({
     return url;
   });
 
-  const [imageRatio, setImageRatio] = useState("1/1");
-
-  const aspectRatioStyle = useDynamicStyle<ImageStyle>(() => {
-    return {
-      width: "100%",
-      aspectRatio: imageRatio,
-    };
-  }, [imageRatio]);
-
   if (!data) {
     return null;
   }
 
   if (originRatio) {
     return (
-      <Image
-        style={aspectRatioStyle}
+      <OriginRatioImage
+        style={style}
         source={{ uri: data }}
-        onLoad={() => {
-          Image.getSize(data, (width, height) => {
-            setImageRatio(`1/${height / width}`);
-          });
-        }}
+        imageUriForCalculateRatio={data}
+        expansion={expansion}
       />
     );
   }
