9197381949f45f65761a2e0ded0f961e46d56f6f
junghyunbak <jeong5728@gmail.com>
2023-11-21 15:39:46 +0900

    feat: 바텀 시트의 레퍼런스 값을 저장할 전역 상태 추가

--- store/index.ts
+++ store/index.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import toast from "./modules/toast";
 import diaryDate from "./modules/diaryDate";
+import bottomSheetRef from "./modules/bottomSheetRef";
 
 const rootReducer = combineReducers({
   toast,
   diaryDate,
+  bottomSheetRef,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
