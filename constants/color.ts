c4daf69eacb1f9fb1d04486be1bb3fccdb0fa9be
junghyunbak <jeong5728@gmail.com>
2023-11-23 11:29:08 +0900

    chore: 컬러 팔레트 추가

--- constants/color.ts
+++ constants/color.ts
@@ -1,7 +1,19 @@
+export const inactive = "#B7B7CB";
+export const agreementClicked = "#1C1D22";
+export const formElementBg = "#EBEBF0";
+
 export const primary = "#8579F1";
 
-export const inactive = "#B7B7CB";
+/**
+ * - input form background
+ * - inactive button background
+ */
+export const g1 = "#EBEBF0";
 
-export const agreementClicked = "#1C1D22";
+/**
+ * - text place holder
+ */
+export const g2 = "#B7B7CB";
 
-export const formElementBg = "#EBEBF0";
+export const error = "#FF8585";
+export const success = "#63BCA9";
