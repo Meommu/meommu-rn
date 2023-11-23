b3cb036ded7695c02e9c7ae26ae001f574504d91
junghyunbak <jeong5728@gmail.com>
2023-11-24 02:31:45 +0900

    test: 테스트를 위한 일기 수정 api 메서드 수정

--- apis/methods/put.ts
+++ apis/methods/put.ts
@@ -4,5 +4,10 @@ export const modifyDiary = async (
   diaryId: string,
   diary: DiaryWriteFormFieldValues
 ): Promise<void> => {
-  await axios.put(`/api/v1/diaries/${diaryId}`, diary);
+  /**
+   * [TEST] vercel.json의 path rewrite 403 error 테스트를 위한 고의적 수정
+   */
+  await axios.put(`/api/v1/diaries/12345678`, diary);
+
+  //await axios.put(`/api/v1/diaries/${diaryId}`, diary);
 };
