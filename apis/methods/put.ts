efba06ce877a426e2df05e6a6f7f207e8a88e6f6
박정현 <jeong5728@gmail.com>
2023-11-24 12:42:54 +0900

    Update put.ts

--- apis/methods/put.ts
+++ apis/methods/put.ts
@@ -7,7 +7,7 @@ export const modifyDiary = async (
   /**
    * [TEST] vercel.json의 path rewrite 403 error 테스트를 위한 고의적 수정
    */
-  await axios.put(`/api/v1/diaries/12345678`, diary);
+  //await axios.put(`/api/v1/diaries/12345678`, diary);
 
-  //await axios.put(`/api/v1/diaries/${diaryId}`, diary);
+  await axios.put(`/api/v1/diaries/${diaryId}`, diary);
 };
