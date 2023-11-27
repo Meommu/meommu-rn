f7e0ef02bb1d0e7623725c2bd842c0a6daf2c864
junghyunbak <jeong5728@gmail.com>
2023-11-27 14:48:24 +0900

    feat: 반응형 바텀시트 모달 레이아웃 구현

---
+++ components/Layout/ResponsiveBottomSheetModal/index.tsx
@@ -0,0 +1,111 @@
+// react
+import React, { useRef, useMemo, useCallback, useEffect } from "react";
+import { useWindowDimensions, Platform, type ViewStyle } from "react-native";
+
+// hooks
+import { useDynamicStyle } from "@/hooks";
+
+// bottom sheet
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetBackdrop,
+  useBottomSheetDynamicSnapPoints,
+  type BottomSheetBackdropProps,
+} from "@gorhom/bottom-sheet";
+
+// constants
+import { size, zIndex } from "@/constants";
+
+// style
+import { styles } from "./index.styles";
+
+interface ResponsiveButtonSheetModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+}
+
+export function ResponsiveButtonSheetModal({
+  isOpen,
+  setIsOpen,
+  children,
+}: ResponsiveButtonSheetModalProps) {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
+  const initialSnapPoints = useMemo(() => ["CONTENT_HEIGHT"], []);
+
+  const {
+    animatedHandleHeight,
+    animatedSnapPoints,
+    animatedContentHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
+
+  const { width, height } = useWindowDimensions();
+
+  const bottomSheetMaxWidthStyle = useDynamicStyle<ViewStyle>(() => {
+    const maxWidth =
+      Platform.OS === "web" && width >= size.LAPTOP_WIDTH
+        ? (9 * height) / 16
+        : "100%";
+
+    return {
+      maxWidth,
+      marginHorizontal: "auto",
+      zIndex: zIndex.bottomSheetContainer,
+    };
+  }, [width, height]);
+
+  useEffect(() => {
+    if (isOpen) {
+      bottomSheetRef.current?.present();
+    } else {
+      bottomSheetRef.current?.dismiss();
+    }
+  }, [isOpen, bottomSheetRef]);
+
+  const handleBottomSheetIndexChange = useCallback(
+    (index: number) => {
+      setIsOpen(index === -1 ? false : true);
+    },
+    [setIsOpen]
+  );
+
+  /**
+   * bottom sheet의 dimmed
+   *
+   * ※ Web 환경에서는 뒷 배경 클릭시 바텀시트 모달이 닫히지 않음.
+   * ※ v5 버전에서 업데이트를 기다려야 할 것 같음.
+   */
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...props}
+        style={[props.style, styles.bottomSheetBackdrop]}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+
+  return (
+    <BottomSheetModal
+      ref={bottomSheetRef}
+      snapPoints={animatedSnapPoints}
+      contentHeight={animatedContentHeight}
+      handleHeight={animatedHandleHeight}
+      enableContentPanningGesture={false}
+      containerStyle={[styles.bottomSheetContainer, bottomSheetMaxWidthStyle]}
+      handleIndicatorStyle={styles.handleIndicator}
+      backdropComponent={renderBackdrop}
+      enablePanDownToClose={true}
+      onChange={handleBottomSheetIndexChange}
+    >
+      <BottomSheetView onLayout={handleContentLayout}>
+        {children}
+      </BottomSheetView>
+    </BottomSheetModal>
+  );
+}
