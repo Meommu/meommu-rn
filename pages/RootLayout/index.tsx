04697e3e707ce6b1ee0b513fb40257dd6b471f8e
junghyunbak <jeong5728@gmail.com>
2023-11-29 15:54:26 +0900

    build: rn 에서는 지원하지 않는 기능을 보충할 polyfill 패키지 설치 및 적용

--- pages/RootLayout/index.tsx
+++ pages/RootLayout/index.tsx
@@ -18,6 +18,22 @@ import { GlobalErrorBoundary } from "./GlobalErrorBoundary";
 import axios from "axios";
 import { MockApiService } from "@/mocking/mockApi";
 
+import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
+import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+
+import { Platform } from "react-native";
+
+/**
+ * rn이 아닌 웹 환경에서 polyfill를 실행 할 경우
+ * 동작에 문제가 생겨 Platform을 확인 후 적용하도록 구현
+ */
+if (Platform.OS !== "web") {
+  polyfillReadableStream();
+  polyfillFetch();
+  polyfillEncoding();
+}
+
 export function RootLayout() {
   /**
    * 폰트, Mocking API, accessToken 설정
