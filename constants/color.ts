0ccd5649cfaff07e5313d19c4f62fde19c39b92e
junghyunbak <jeong5728@gmail.com>
2023-12-02 12:32:29 +0900

    chore: 팔레드 색상 정리

--- constants/color.ts
+++ constants/color.ts
@@ -1,26 +1,33 @@
+export const w = "#ffffff";
+
+export const g100 = "#F5F6F8";
+export const g200 = "#EBEBF0";
+export const g300 = "#BBBBC9";
+export const g400 = "#89899C";
+export const g500 = "#666675";
+export const g600 = "#555563";
+export const g700 = "#4E4E5B";
+export const g800 = "#373840";
+export const g900 = "#1C1D22";
+
+export const bg100 = "#B7B8BA";
+export const bg200 = "#6F7682";
+export const bg300 = "#444A56";
+export const bg400 = "#363C48";
+export const bg500 = "#1B1E26";
+
+export const b = "#000000";
+
+export const error = "#FF8585";
+export const success = "#63BCA9";
+export const primary = "#8579F1";
+
+/**
+ * 삭제될 색상들
+ */
 export const inactive = "#B7B7CB";
 export const agreementClicked = "#1C1D22";
 export const formElementBg = "#EBEBF0";
 
-export const primary = "#8579F1";
-
-/**
- * - input form background
- * - inactive button background
- */
 export const g1 = "#EBEBF0";
-
-/**
- * - text place holder
- */
 export const g2 = "#B7B7CB";
-
-export const g300 = "#BBBBC9";
-export const g400 = "#89899C";
-export const g800 = "#373840";
-export const g900 = "#1C1D22";
-
-export const error = "#FF8585";
-export const success = "#63BCA9";
-
-export const w = "#ffffff";
