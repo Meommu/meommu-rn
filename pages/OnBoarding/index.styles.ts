811dc3a643490bd5c69561a550c82a6843a9b782
junghyunbak <jeong5728@gmail.com>
2023-12-03 13:12:43 +0900

    style: onboarding 레이아웃, 스타일 상수 교체

--- pages/OnBoarding/index.styles.ts
+++ pages/OnBoarding/index.styles.ts
@@ -1,13 +1,19 @@
 // react
 import { StyleSheet } from "react-native";
 
+// constants
+import { color, font } from "@/constants";
+
 export const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
-    backgroundColor: "white",
+    backgroundColor: color.w,
   },
 
+  /**
+   * swiper
+   */
   swiper: {
     /**
      * ※ width 값을 고정시키면 슬라이더가 동작하지 않음
@@ -18,26 +24,21 @@ export const styles = StyleSheet.create({
   slideLayout: {
     width: "100%",
     height: "100%",
-    position: "relative",
-    justifyContent: "center",
-    alignContent: "center",
   },
 
   guideText: {
     fontSize: 25,
-    fontFamily: "Pretendard-SemiBold",
-    position: "absolute",
-    left: 20,
-    top: 64,
-    zIndex: 1,
+    fontFamily: font.PretendardSemiBold,
+
+    paddingHorizontal: 20,
+    paddingTop: 52,
   },
 
-  buttonView: {
-    width: "100%",
-    padding: 20,
+  dotContainer: {
+    margin: 0,
   },
 
-  dotStyle: {
+  dot: {
     marginLeft: 3.75,
     marginRight: 3.75,
     width: 6,
@@ -47,5 +48,14 @@ export const styles = StyleSheet.create({
      * minWidth 속성 default(8)값을 제거해주기 위함
      */
     minWidth: 0,
+  },
+
+  /**
+   * bottom button
+   */
+  buttonView: {
+    width: "100%",
+    padding: 20,
+    paddingTop: 6,
   },
 });
--- pages/OnBoarding/index.tsx
+++ pages/OnBoarding/index.tsx
@@ -11,7 +11,7 @@ import { NavigationButton } from "@/components/Button/NavigationButton";
 import { BannerImage } from "@/components/Image/BannerImage";
 
 // constants
-import { PATH } from "@/constants";
+import { PATH, color } from "@/constants";
 
 // hooks
 import { useSwiper } from "@/hooks";
@@ -57,10 +57,10 @@ export function OnBoardingPage() {
         }}
         controlsProps={{
           dotsTouchable: true,
-          dotActiveStyle: { ...styles.dotStyle, backgroundColor: "#565667" },
+          dotActiveStyle: { ...styles.dot, backgroundColor: color.g600 },
           dotProps: {
-            badgeStyle: { ...styles.dotStyle, backgroundColor: "#CCCCD9" },
-            containerStyle: { margin: 0 },
+            containerStyle: styles.dotContainer,
+            badgeStyle: { ...styles.dot, backgroundColor: color.g300 },
           },
           nextPos: false,
           prevPos: false,
@@ -68,34 +68,34 @@ export function OnBoardingPage() {
         onIndexChanged={handleSwiperIndexChange}
       >
         <View style={styles.slideLayout}>
-          <Text style={styles.guideText}>
-            추억하고 싶은 순간을{"\n"}멈무일기에서 기록해봐요!
-          </Text>
-
           <BannerImage
             source={require("@/assets/images/onboarding/onboarding-1.png")}
           />
+
+          <Text style={styles.guideText}>
+            추억하고 싶은 순간을{"\n"}멈무일기에서 기록해봐요!
+          </Text>
         </View>
 
         <View style={styles.slideLayout}>
+          <BannerImage
+            source={require("@/assets/images/onboarding/onboarding-2.png")}
+          />
+
           <Text style={styles.guideText}>
             활동을 카테고리에 따라{"\n"}
             GPT로 간단하게 완성해봐요!
           </Text>
-
-          <BannerImage
-            source={require("@/assets/images/onboarding/onboarding-2.png")}
-          />
         </View>
 
         <View style={styles.slideLayout}>
-          <Text style={styles.guideText}>
-            글과 사진을 함께 구성하며{"\n"}지루하지 않게 꾸며봐요!
-          </Text>
-
           <BannerImage
             source={require("@/assets/images/onboarding/onboarding-3.png")}
           />
+
+          <Text style={styles.guideText}>
+            글과 사진을 함께 구성하며{"\n"}지루하지 않게 꾸며봐요!
+          </Text>
         </View>
       </Swiper>
 
