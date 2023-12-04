dc67908d9c92625c132b7676768b269668e781bc
junghyunbak <jeong5728@gmail.com>
2023-12-04 18:22:11 +0900

    chore: 상수 추가

--- constants/size.ts
+++ constants/size.ts
@@ -1,13 +1,11 @@
 export const NAVIGATION_BUTTON_HEIGHT = 48;
-
 export const AI_BOTTOM_SHEET_HEADER_HEIGHT = 80;
-
 export const BOTTOM_SHEET_INDICATOR_HEIGHT = 24;
+export const MONTH_CALENDAR_HEIGHT = 400;
+export const LINEAR_GRADIENT_HEIGHT = 24;
+
+export const FOOTER_PADDING_BOTTOM = 13;
 
 export const MOBILE_WIDTH = 425;
-
 export const TABLET_WIDTH = 768;
-
 export const LAPTOP_WIDTH = 1024;
-
-export const MONTH_CALENDAR_HEIGHT = 400;
