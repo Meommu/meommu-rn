2c90cff4c8e9abc6514fdaf0d091888cc720ea4c
junghyunbak <jeong5728@gmail.com>
2023-12-04 18:49:36 +0900

    chore: 필요없어진 `/app/setting/profile`의 suspense 제거

--- app/setting/profile/index.tsx
+++ app/setting/profile/index.tsx
@@ -1,23 +1,10 @@
 import { ResponsiveKeyboardAvoidingView } from "@/components/Layout/ResponsiveKeyboardAvoidingView";
 import { ProfilePage } from "@/pages/Setting/Profile";
-import { Suspense } from "react";
-import { View, Text } from "react-native";
 
 export default function Profile() {
   return (
     <ResponsiveKeyboardAvoidingView>
-      <Suspense
-        fallback={
-          /**
-           * TODO: 스켈레톤 UI 구현하여 적용
-           */
-          <View>
-            <Text>로딩중...</Text>
-          </View>
-        }
-      >
-        <ProfilePage />
-      </Suspense>
+      <ProfilePage />
     </ResponsiveKeyboardAvoidingView>
   );
 }
