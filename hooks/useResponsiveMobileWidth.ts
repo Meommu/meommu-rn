23cc60d77627e95b7cf98c7226b95a4371754837
junghyunbak <jeong5728@gmail.com>
2023-12-06 01:00:41 +0900

    chore: 훅 내부 로직 변경에 따른 주석 수정

--- hooks/useResponsiveMobileWidth.ts
+++ hooks/useResponsiveMobileWidth.ts
@@ -11,11 +11,11 @@ import { useDynamicStyle } from "./useDynamicStyle";
 
 export const useResponsiveMobileWidth = () => {
   /**
-   * expo-router 특성상 현재 페이지가 아니더라도 메모리에 렌더링 되어있을 수 있다.
+   * ※ expo-router 특성상 현재 페이지가 아니더라도 메모리에 렌더링 되어있을 수 있다.
    *
-   * `useWindowDimensions` 훅을 사용하는 만큼 화면이 크기조절 될 경우 리렌더링이 발생할 수 있으니,
-   *
-   * 리렌더링으로 인한 오작동이 발생하지 않게 훅을 사용하는 컴포넌트를 잘 고려하며 사용해야 한다.
+   * 루트 레이아웃의 width값은 `/pages/RootLayout/ResponsiveLayoutView` 컴포넌트에서
+   * `useWindowDimensions` 훅을 사용해 계산된 후 전역 상태로 공유되는 것이므로,
+   * 해당 훅을 사용하는 컴포넌트는 화면이 크기조절 될 경우 리렌더링 될 수 있다는 것을 고려해 오작동이 발생하지 않게 유의하여 사용해야 한다.
    */
   const { width } = useSelector<RootState, LayoutState>(
     (state) => state.layout
