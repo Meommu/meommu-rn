3cc46b3c57fd24abf3a2a5d1dff4b791f972539c
junghyunbak <jeong5728@gmail.com>
2023-12-08 13:24:43 +0900

    chore: 웹에서 사용될 title, meta 태그 추가
    - meta 태그 내용은 임시로 작성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
+// expo
+import Head from "expo-router/head";
+
 // components
 import { RootLayout } from "@/pages/RootLayout";
 
 export default function AppLayout() {
-  return <RootLayout />;
+  return (
+    <>
+      <Head>
+        <title>Meommu</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="공유시 보여질 이미지 경로" />
+        <meta property="og:title" content="공유시 보여질 제목" />
+        <meta property="og:description" content="공유시 보여질 설명" />
+        <meta property="og:url" content="공유시 이동 url" />
+      </Head>
+      <RootLayout />
+    </>
+  );
 }
