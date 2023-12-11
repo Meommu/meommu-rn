ea1f2db011820a5737273dfab3e42f5fa72ea6df
junghyunbak <jeong5728@gmail.com>
2023-12-11 13:52:15 +0900

    feat: 바텀 버튼 ui 인터렉션 추가

--- components/Button/NavigationButton/index.styles.ts
+++ components/Button/NavigationButton/index.styles.ts
@@ -8,11 +8,35 @@ export const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: size.NAVIGATION_BUTTON_HEIGHT,
-    flexShrink: 1,
-    justifyContent: "center",
-    alignItems: "center",
 
     borderRadius: 6,
+
+    overflow: "hidden",
+
+    position: "relative",
+  },
+
+  content: {
+    width: "100%",
+    height: "100%",
+    flexShrink: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 7,
+  },
+
+  dimmed: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+
+    width: "100%",
+    height: "100%",
+
+    pointerEvents: "none",
+
+    backgroundColor: color.g800,
   },
 
   buttonText: {
--- components/Button/NavigationButton/index.tsx
+++ components/Button/NavigationButton/index.tsx
@@ -1,5 +1,15 @@
 // react
-import { Text, Pressable, type PressableProps } from "react-native";
+import {
+  Text,
+  Pressable,
+  type PressableProps,
+  ActivityIndicator,
+} from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 // constants
 import { color } from "@/constants";
@@ -24,19 +34,47 @@ export function NavigationButton({
 
   ...props
 }: NavigationButtonProps) {
-  const backgroundColor = disabled
-    ? color.g300
-    : isLoading
-    ? color.primaryB
-    : color.primary;
+  const backgroundColor = disabled ? color.g300 : color.primary;
+
+  const scale = useSharedValue(1);
+  const opacity = useSharedValue(0);
+
+  const containerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: withTiming(scale.value, { duration: 100 }) }],
+    };
+  });
+
+  const dimmedAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(opacity.value, { duration: 100 }),
+    };
+  });
+
+  const handleButtonPressIn = () => {
+    scale.value = 0.98;
+    opacity.value = 0.1;
+  };
+
+  const handleButtonPressOut = () => {
+    scale.value = 1;
+    opacity.value = 0;
+  };
 
   return (
-    <Pressable
-      style={[styles.container, { backgroundColor }]}
-      disabled={disabled || isLoading}
-      {...props}
-    >
-      <Text style={styles.buttonText}>{content}</Text>
-    </Pressable>
+    <Animated.View style={[styles.container, containerAnimatedStyle]}>
+      <Pressable
+        style={[styles.content, { backgroundColor }]}
+        disabled={disabled || isLoading}
+        onPressIn={handleButtonPressIn}
+        onPressOut={handleButtonPressOut}
+        {...props}
+      >
+        {isLoading && <ActivityIndicator color={color.primaryB} />}
+        <Text style={styles.buttonText}>{content}</Text>
+      </Pressable>
+
+      <Animated.View style={[styles.dimmed, dimmedAnimatedStyle]} />
+    </Animated.View>
   );
 }
