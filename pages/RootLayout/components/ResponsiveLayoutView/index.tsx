a11958094515062b56a705c3d6a43e2eb7845311
junghyunbak <jeong5728@gmail.com>
2023-12-14 13:08:40 +0900

    refactor: useWindowDimension 훅 제거, 미디어 쿼리 추가

--- pages/RootLayout/components/ResponsiveLayoutView/index.tsx
+++ pages/RootLayout/components/ResponsiveLayoutView/index.tsx
@@ -1,71 +1,20 @@
 // react
-import React, { useCallback } from "react";
-import {
-  View,
-  useWindowDimensions,
-  Platform,
-  type LayoutChangeEvent,
-  type ViewStyle,
-} from "react-native";
+import React from "react";
+import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-// redux
-import { useDispatch } from "react-redux";
-import { changeLayoutWidth } from "@/store/modules/layout";
-
-// constants
-import { size } from "@/constants";
-
-// hooks
-import { useDynamicStyle } from "@/hooks";
-
 // styles
-import { styles } from "./index.styles";
+import { ids, styles } from "./index.styles";
 
 interface ResponsiveLayoutViewProps {
   children: React.ReactNode;
 }
 
 export function ResponsiveLayoutView({ children }: ResponsiveLayoutViewProps) {
-  const dispatch = useDispatch();
-
   const { top } = useSafeAreaInsets();
 
-  const { width } = useWindowDimensions();
-
-  const responsiveStyle = useDynamicStyle<ViewStyle>(() => {
-    if (Platform.OS !== "web" || width < size.LAPTOP_WIDTH) {
-      return {
-        width: "100%",
-        height: "100%",
-      };
-    }
-
-    return {
-      width: "auto",
-      maxWidth: "100%",
-      height: "100%",
-      aspectRatio: "9 / 16",
-
-      marginHorizontal: "auto",
-
-      overflow: "hidden",
-    };
-  }, [width]);
-
-  const handleLayoutChange = useCallback(
-    ({
-      nativeEvent: {
-        layout: { width },
-      },
-    }: LayoutChangeEvent) => {
-      dispatch(changeLayoutWidth(width));
-    },
-    []
-  );
-
   return (
-    <View style={responsiveStyle} onLayout={handleLayoutChange}>
+    <View style={styles.container} dataSet={{ media: ids.container }}>
       <View style={[styles.notch, { height: top }]} />
 
       <View style={styles.content}>{children}</View>
