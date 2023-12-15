a04cecd7532b68f704cd6e1f48d5744744d440f8
junghyunbak <jeong5728@gmail.com>
2023-12-16 02:27:16 +0900

    feat: 헤더에 notch 영역 on/off 옵션 추가

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -3,20 +3,44 @@ import React from "react";
 import { View, Text, type ViewProps } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+// hooks
+import { useDynamicStyle } from "@/hooks";
+
 // styles
 import { styles } from "./index.styles";
 
 interface HeaderProps extends ViewProps {
   title?: string | React.ReactNode;
+
   left?: React.ReactNode;
+
   right?: React.ReactNode;
+
+  notch?: boolean;
 }
 
-export function Header({ title, left, right, style, ...props }: HeaderProps) {
+export function Header({
+  title,
+  left,
+  right,
+  notch = true,
+  style,
+  ...props
+}: HeaderProps) {
   const { top } = useSafeAreaInsets();
 
+  const notchStyle = useDynamicStyle(() => {
+    if (notch) {
+      return {
+        marginTop: top,
+      };
+    }
+
+    return {};
+  }, [notch, top]);
+
   return (
-    <View style={[style, styles.container, { marginTop: top }]} {...props}>
+    <View style={[style, styles.container, notchStyle]} {...props}>
       <View style={styles.controller}>
         {left || <View />}
         {right || <View />}
